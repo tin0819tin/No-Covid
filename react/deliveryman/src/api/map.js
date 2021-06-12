dfa4cf515a88e2bf47d68829bffb4c604186cd5c
TIN <b06502141@g.ntu.edu.tw>
2021-06-12 22:36:06 +0800

    Adding API utils file

---
+++ react/deliveryman/src/api/map.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import { Loader } from "@googlemaps/js-api-loader"
+
+
+const loader = new Loader({
+    apiKey: "AIzaSyCWz2zqy-WkQ24m1kAAz-rp_viKj4BEv3g",
+    version: "weekly"
+
+});
+
+export default loader;
